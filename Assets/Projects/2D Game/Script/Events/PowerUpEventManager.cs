0ce74ce69d0108ebfb3ebf2d048de7e717b79e1f
Jonathan Pacheco <cdmx2508@amerike.edu.mx>
2025-10-28 14:02:29 -0600

    Update Unity packages and editor version
    Upgraded several Unity packages in manifest.json to newer versions, including collab-proxy, inputsystem, render-pipelines, textmeshpro, timeline, and visualscripting. Updated ProjectVersion.txt to Unity editor version 2022.3.62f2. Also removed commented code from PowerUpEventManager.cs for clarity.

--- Assets/Projects/2D Game/Script/Events/PowerUpEventManager.cs
+++ Assets/Projects/2D Game/Script/Events/PowerUpEventManager.cs
@@ -2,14 +2,10 @@ using System;
 using UnityEngine;
 public class PowerUpEventManager : MonoBehaviour
 {
-    // Evento para activar Triple Shot
     public static event Action OnTripleShotActivated;
     public static event Action OnTripleShotDesactivated;
-
-    // Evento para activar Shield
     public static event Action OnShieldActivated;
 
-    // ---- MÉTODOS PARA INVOCAR LOS EVENTOS ----
     public static void TriggerTripleShotActivated()
     {
         OnTripleShotActivated?.Invoke();
